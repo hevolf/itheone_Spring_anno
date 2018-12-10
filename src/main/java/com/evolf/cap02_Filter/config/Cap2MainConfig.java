d985e40994771c94a16a3ebc05bfeb4e111f9e53
hevolf <caohaifengx@163.com>
2018-12-10 23:29:29 +0800

    Spring_anno filter、scoper、lazy

--- src/main/java/com/evolf/cap02_Filter/config/Cap2MainConfig.java
+++ src/main/java/com/evolf/cap02_Filter/config/Cap2MainConfig.java
@@ -3,6 +3,7 @@ package com.evolf.cap02_Filter.config;
 import com.evolf.cap01.Person;
 import com.evolf.cap02_Filter.controller.OrderController;
 import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 //扫描所有组件 service controller 等等 @ComponentScan
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.*;
 //        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
 //}
 //useDefaultFilters = false 不适用默认配置
-//@ComponentScan(value = "com.evolf.cap02_Filter", excludeFilters = {
-//        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})//注解类型
-//}, useDefaultFilters = false)
+@ComponentScan(value = "com.evolf.cap02_Filter", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})//注解类型
+}, useDefaultFilters = false) //excludeFilters 不扫描@Controller，useDefaultFilters = false所以默认没有要扫描的注解 //include有指定的扫描
+
 
 //@ComponentScan(value = "com.evolf.cap02_Filter", includeFilters = {
 //        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {OrderController.class})//指定类型
@@ -23,15 +25,15 @@ import org.springframework.context.annotation.*;
 //}, useDefaultFilters = false)
 
 
-@ComponentScans(
-        value = {@ComponentScan(value = "com.evolf.cap02_Filter", includeFilters = {
-                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = {com.evolf.cap02_Filter.config.JamesTypeFilter.class})//自定义类型
-        }, useDefaultFilters = false)
-                ,
-                @ComponentScan(value = "com.evolf.cap02_Filter", includeFilters = {
-                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {OrderController.class})//指定类型
-                }, useDefaultFilters = false)}
-)
+//@ComponentScans(
+//        value = {@ComponentScan(value = "com.evolf.cap02_Filter", includeFilters = {
+//                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = {com.evolf.cap02_Filter.config.JamesTypeFilter.class})//自定义类型
+//        }, useDefaultFilters = false)
+//                ,
+//                @ComponentScan(value = "com.evolf.cap02_Filter", includeFilters = {
+//                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {OrderController.class})//指定类型
+//                }, useDefaultFilters = false)}
+//)
 public class Cap2MainConfig {
     ////给容器中注册一个bean, 类型为返回值的类型, beanId名称未指定时默认为方法名
     @Bean
