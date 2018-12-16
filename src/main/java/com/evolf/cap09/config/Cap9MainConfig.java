2e9c604f4f847a789e6f25abb7307fd48d780116
hevolf <caohaifengx@163.com>
2018-12-17 00:19:27 +0800

    Spring_anno init

---
+++ src/main/java/com/evolf/cap09/config/Cap9MainConfig.java
@@ -0,0 +1,21 @@
+package com.evolf.cap09.config;
+
+import com.evolf.cap09.dao.TestDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+@ComponentScan({"com.evolf.cap09.controller","com.evolf.cap09.service","com.evolf.cap09.dao"})
+public class Cap9MainConfig {
+	//spring进行自装配的时候默认首选的bean
+	//@Primary
+	@Bean("testDao")
+	public TestDao testDao(){
+		TestDao testDao = new TestDao();
+		testDao.setFlag("2");
+		return testDao;
+	}
+}
