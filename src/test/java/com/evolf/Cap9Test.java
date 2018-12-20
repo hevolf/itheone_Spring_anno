224543b4d473f0eb06be8defa64b8ce804664fb5
hevolf <caohaifengx@163.com>
2018-12-20 23:57:34 +0800

    Spring_anno bean优先级

---
+++ src/test/java/com/evolf/Cap9Test.java
@@ -0,0 +1,26 @@
+package com.evolf.cap09.service;
+
+import com.evolf.cap09.config.Cap9MainConfig;
+import com.evolf.cap09.dao.TestDao;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Cap9Test {
+	@Test
+	public void test01(){
+		AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap9MainConfig.class);
+		String[] names = app.getBeanDefinitionNames();
+		for (String name : names) {
+			System.out.println(name);
+		}
+
+		TestService testService = app.getBean(TestService.class);
+		testService.println();//打印的是service中的testDao2
+		//直接从容器中获取TestDao, 和使用Autowired注解来取比较
+		TestDao testDao = app.getBean(TestDao.class);//存在testDao  testDao2  不唯一 取出会报错
+		System.out.println(testDao);
+
+		//关闭容器
+		app.close();
+	}
+}
